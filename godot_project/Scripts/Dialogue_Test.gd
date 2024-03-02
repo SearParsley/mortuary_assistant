f9cca2a09a96daa3f3a0927b64c6becbacf2de1a
SearParsley <searparsley@gmail.com>
2024-03-02 06:13:30 -0700

    tests for influencer dialogue

--- godot_project/Scripts/Dialogue_Test.gd
+++ godot_project/Scripts/Dialogue_Test.gd
@@ -1,5 +1,5 @@
 extends Node3D
 
 func _ready():
-	var resource = load("res://dialogue/test.dialogue")
-	DialogueManager.show_dialogue_balloon(resource)
+	var resource = load("res://dialogue/influencer_test.dialogue")
+	DialogueManager.show_dialogue_balloon(resource, "")
