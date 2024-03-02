4812c88aef6302161bcc2c0991e048df047fa79a
SearParsley <searparsley@gmail.com>
2024-03-02 14:37:00 -0700

    tooltips

--- godot_project/Scripts/CompleteBasket.gd
+++ godot_project/Scripts/CompleteBasket.gd
@@ -2,5 +2,8 @@ extends StaticBody3D
 
 signal submit_form
 
+func returnMessage():
+	return "Place Form"
+
 func interact_action():
 	submit_form.emit()
--- godot_project/Scripts/ToDoBasket.gd
+++ godot_project/Scripts/ToDoBasket.gd
@@ -2,5 +2,8 @@ extends StaticBody3D
 
 signal new_form
 
+func returnMessage():
+	return "New Form"
+
 func interact_action():
 	new_form.emit()
