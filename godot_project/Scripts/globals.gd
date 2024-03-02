fbf429b600c21428d192127632d5bf64f4844fba
SearParsley <searparsley@gmail.com>
2024-03-02 12:50:37 -0700

    added sprite path to ghost dict

--- godot_project/Scripts/globals.gd
+++ godot_project/Scripts/globals.gd
@@ -4,47 +4,56 @@ extends Node
 	"jimmy": {
 		"dialogue_path": "res://dialogue/influencer_test.dialogue",
 		"dialogue_resource": "",
-		"has_met": false
+		"has_met": false,
+		"sprite_path": "",
 	},
 	"ghost2": {
 		"dialogue_path": "res://dialogue/test.dialogue",
 		"dialogue_resource": "",
-		"has_met": false
+		"has_met": false,
+		"sprite_path": "",
 	},
 	"ghost3": {
 		"dialogue_path": "res://dialogue/test.dialogue",
 		"dialogue_resource": "",
-		"has_met": false
+		"has_met": false,
+		"sprite_path": "",
 	},
 	"ghost4": {
 		"dialogue_path": "res://dialogue/test.dialogue",
 		"dialogue_resource": "",
-		"has_met": false
+		"has_met": false,
+		"sprite_path": "",
 	},
 	"ghost5": {
 		"dialogue_path": "res://dialogue/test.dialogue",
 		"dialogue_resource": "",
-		"has_met": false
+		"has_met": false,
+		"sprite_path": "",
 	},
 	"ghost6": {
 		"dialogue_path": "res://dialogue/test.dialogue",
 		"dialogue_resource": "",
-		"has_met": false
+		"has_met": false,
+		"sprite_path": "",
 	},
 	"ghost7": {
 		"dialogue_path": "res://dialogue/test.dialogue",
 		"dialogue_resource": "",
-		"has_met": false
+		"has_met": false,
+		"sprite_path": "",
 	},
 	"ghost8": {
 		"dialogue_path": "res://dialogue/test.dialogue",
 		"dialogue_resource": "",
-		"has_met": false
+		"has_met": false,
+		"sprite_path": "",
 	},
 	"ghost9": {
 		"dialogue_path": "res://dialogue/test.dialogue",
 		"dialogue_resource": "",
-		"has_met": false
+		"has_met": false,
+		"sprite_path": "",
 	},
 }
 
